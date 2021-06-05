c2e4fe1d1c526737004af53fc5ddf406128d7312
Omar Hazim <79670252+haziminyo@users.noreply.github.com>
2021-06-05 20:04:14 +0300

    main...

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.haziminyo;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+       var tree = new AVLTree();
+        tree.insert(10);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(18);
+      
+      // To it's work should be run it with Debug 'Main'
+
+     }
+
+    }
